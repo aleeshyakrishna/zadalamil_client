304d9ad5cd44b376e36dc19e0724d5d581b8dbd4
Akhila Vijayan <akhilavijayan964@gmail.com>
2025-01-02 13:15:02 +0400

    xl

--- src/pages/Admin/VendorDetails.jsx
+++ src/pages/Admin/VendorDetails.jsx
@@ -12,7 +12,7 @@ const VendorDetailsPage = () => {
                     <SidebarWithBurgerMenu />
                 </div>
 
-                <div className="ml-16 p-4 w-full -mt-20 lg:-mt-5">
+                <div className="ml-16 p-4 w-full -mt-20 lg:-mt-5 xl:-mt-0">
                 <VendorDetails />
                 </div>
             </div>
