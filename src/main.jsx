518b28f8f3be1d3002a0077e47f7a14347603cef
Akhila Vijayan <akhilavijayan964@gmail.com>
2025-01-24 13:04:55 +0530

    toast customization

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-//import { ToastContainer } from "react-toastify";
+import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './Redux/store.js';
 import { Toaster } from 'react-hot-toast';
@@ -12,12 +11,14 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store} >
    <App />
    </Provider>
-    {/* <ToastContainer /> */}
     <Toaster
       toastOptions={{
         style: {
-          backgroundColor: 'black', 
+          background: 'linear-gradient(45deg, red, black)',
           color: 'white', 
+          padding: '10px 20px',
+          fontSize: '16px',
+          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
         },
       }}
      />
